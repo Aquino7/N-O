dc16070482ef0d6d458eca58118c838daf82de43
Aquino7 <brendaaquino037@gmail.com>
2022-06-14 12:20:55 -0500

    alteração

--- cadastro.php
+++ cadastro.php
@@ -2,3 +2,11 @@
 //conexão com o banco de dados via include
 include (conexao.php);
 
+$nome = $_post['name']; 
+$nascimento = $_post['date'];
+$email = $_post['email'];
+$senha = $_post['password'];
+
+$sql = "INSERT INTO usuarios(nome, nascimento, email, senha) VALUES ('$name, '$nascimento', '$email', '$senha')";
+
+if(mysqli_query($conexao,$sql)
